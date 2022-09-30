cbdb06126ef9b41ff910fe14ac27691faf821cc1
Davin Howarth <davinhow2@gmail.com>
2022-09-30 01:31:45 -0400

    visual reference for auto mode

---
+++ Unfinished Business (Spooktober 2022)/Assets/Scripts/Auto.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Auto : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI t;
+
+    static bool auto;
+
+    private void Start()
+    {
+        switch (auto)
+        {
+            case true:
+                t.color = Color.green;
+                break;
+            case false:
+                t.color = Color.white;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            auto = !auto;
+            
+            switch (auto)
+            {
+                case true: t.color = Color.green;
+                    break;
+                case false: t.color = Color.white;
+                    break;
+            }
+        }
+    }
+}
